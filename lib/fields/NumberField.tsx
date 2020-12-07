8ad89a3f0046b6fb198275d02e02a2105927d4da
Chris <757541811@qq.com>
2020-12-07 20:56:59 +0800

    SchemaForm、SchemaFormItem

---
+++ lib/fields/NumberField.tsx
@@ -0,0 +1,8 @@
+import {defineComponent, PropType} from 'vue'
+
+export default defineComponent({
+  name: 'NumberField',
+  setup(){
+    return ()=><div>Number field</div>
+  }
+})
---
+++ lib/fields/StringField.tsx
@@ -0,0 +1,8 @@
+import {defineComponent, PropType} from 'vue'
+
+export default defineComponent({
+  name: 'StringField',
+  setup(){
+    return ()=><div>String field</div>
+  }
+})
