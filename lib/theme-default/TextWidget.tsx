d8196f194c03ad661200b01e15f7f17023fca9b7
Chris <757541811@qq.com>
2020-12-08 22:12:59 +0800

    vue实现受控组件的方式

--- lib/theme-default/TextWidget.tsx
+++ lib/theme-default/TextWidget.tsx
@@ -1,11 +1,24 @@
 import {CommonWidgetDefine, CommonWidgetPropsDefine} from "../types";
-import {defineComponent} from "vue";
+import {defineComponent, nextTick} from "vue";
 
 const TextWidget: CommonWidgetDefine = defineComponent({
   props: CommonWidgetPropsDefine,
   setup(props) {
     const handleChange = (e: any) => {
+      // console.log(e);
+      // props.onChange(e.target.value)
+
+      /** 实现受控组件 法一*/
+      /*nextTick(()=>{
+        if (props.value !== e.target.value) {
+          e.target.value = props.value
+        }
+      })*/
+
+
+      /** 实现受控组件 法二*/
       console.log(e);
+      e.target.value = props.value
       props.onChange(e.target.value)
     };
 
